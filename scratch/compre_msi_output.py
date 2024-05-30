935b6a99de808ad98eb3c51875287f0cdb033480
Devin McCabe <dmccabe@broadinstitute.org>
2024-05-30 12:27:47 -0400

    read dis output files

--- scratch/compre_msi_output.py
+++ scratch/compre_msi_output.py
@@ -1,4 +1,5 @@
 import os
+from functools import reduce
 
 import numpy as np
 import pandas as pd
@@ -70,7 +71,9 @@ def save_msi_outputs(storage_client, df, old_or_new):
             blob = storage.Blob.from_string(r[c], client=storage_client)
             blob.download_to_filename(local_file)
 
-    pass
+            df[c] = local_file
+
+    return df
 
 
 def compare_scores(old_samples, new_samples):
@@ -80,14 +83,73 @@ def compare_scores(old_samples, new_samples):
         how="inner",
         suffixes=("_old", "_new"),
     )
+
     score_med_abs_dev = np.abs(
         score_comp["msisensor2_score_new"] - score_comp["msisensor2_score_old"]
     ).median()
     score_max_abs_dev = np.abs(
         score_comp["msisensor2_score_new"] - score_comp["msisensor2_score_old"]
     ).max()
+
     print(f"Median absolute deviation: {round(score_med_abs_dev, 2)}")
     print(f"Max absolute deviation: {round(score_max_abs_dev, 2)}")
+
+
+def get_weighted_avg(dis):
+    dis = pd.Series(dis.replace("T:", "").strip().split(" ")).astype(int)
+    return np.ma.average(dis.index + 1, weights=dis.values)
+
+
+def get_n_repeats_from_msisensor2_output_dis(path):
+    df = pd.read_csv(path, sep="\t", header=None)
+
+    # split site rows from distribution rows (they alternate)
+    df = pd.DataFrame(
+        {
+            "site": df.loc[df.index % 2 == 0, 0].values,
+            "dis": df.loc[df.index % 2 == 1, 0].values,
+        }
+    )
+
+    # split the site into its component info
+    site_cols = ["chr", "loc", "left_flank", "repeat", "right_flank"]
+
+    df = pd.concat(
+        [
+            pd.DataFrame(df["site"].str.split(" ").tolist(), columns=site_cols),
+            df["dis"],
+        ],
+        axis=1,
+    )
+
+    # calc weighted average number of repeats
+    df["wavg"] = df["dis"].apply(get_weighted_avg)
+    return df
+
+
+def combine_dis_wavg(df):
+    repeats = [
+        get_n_repeats_from_msisensor2_output_dis(x["msisensor2_output_dis"])
+        .drop(columns="dis")
+        .rename(columns={"wavg": x["sample_id"]})
+        .set_index(["chr", "loc", "left_flank", "repeat", "right_flank"])
+        for _, x in df.iterrows()
+    ]
+
+    # reduce a list of data frames into a single dataframe using `join`
+    return reduce(lambda x, y: x.join(y, how="outer"), repeats)
+
+
+def compare_repeats(old_samples, new_samples):
+    old_repeats = combine_dis_wavg(old_samples)
+    new_repeats = combine_dis_wavg(new_samples)
+
+    repeats_comp = old_repeats.join(
+        new_repeats, how="outer", lsuffix="_old", rsuffix="_new"
+    )
+
+    # make sure the outer join was effectively an inner one
+    assert len(repeats_comp) == len(old_repeats) == len(new_repeats)
 
 
 old_samples = get_terra_outputs(
@@ -102,7 +164,7 @@ old_samples = old_samples.loc[old_samples["sample_id"].isin(sample_ids)]
 new_samples = new_samples.loc[new_samples["sample_id"].isin(sample_ids)]
 
 storage_client = storage.Client(project="depmap-omics")
-save_msi_outputs(storage_client, old_samples, "old")
-save_msi_outputs(storage_client, new_samples, "new")
+old_samples = save_msi_outputs(storage_client, old_samples, "old")
+new_samples = save_msi_outputs(storage_client, new_samples, "new")
 
 compare_scores(old_samples, new_samples)
