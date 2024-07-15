87f2d7c115f88e75f737f316fd99cdeac25aa815
Devin McCabe <dmccabe@broadinstitute.org>
2024-07-15 14:38:46 -0400

    dna repair genes

---
+++ workflows/annotate_mutations/scripts/dna_repair_genes.py
@@ -0,0 +1,26 @@
+import datetime
+
+import pandas as pd
+from _csv import QUOTE_NONE
+
+df = pd.read_table(
+    "https://storage.googleapis.com/broad-public-datasets/funcotator/funcotator_dataSources.v1.7.20200521s/dna_repair_genes/hg19/dnaRepairGenes.20180524T145835.csv",
+    sep="|",
+    quoting=QUOTE_NONE,
+    dtype="string",
+    on_bad_lines=lambda x: x[:4],
+    engine="python",
+)
+
+df = df.rename(
+    {
+        "Gene Name": "gene_name",
+        "Activity linked to OMIM": "activity",
+        "Chromosome location linked to NCBI MapView": "chrom_pos",
+        "Accession number linked to NCBI Entrez": "accession_number",
+    }
+)
+
+today = datetime.datetime.today().strftime("%Y-%m-%d")
+
+df.to_csv(f"./data/dna_repair_genes_{today}.csv", index=False)
