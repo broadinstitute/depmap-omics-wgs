b5da254c38d98b509b8a1da0d3a8a0d82a8bb22d
Devin McCabe <dmccabe@broadinstitute.org>
2025-01-02 14:28:35 -0500

    expose more postprocess options

--- workflows/annotate_mutations/annotate-mutations-postprocess/annotate_mutations_postprocess/__main__.py
+++ workflows/annotate_mutations/annotate-mutations-postprocess/annotate_mutations_postprocess/__main__.py
@@ -39,9 +39,19 @@ def duckdb_to_maf(
     db: Annotated[Path, typer.Option()],
     parquet_dir: Annotated[Path, typer.Option()],
     out_file: Annotated[Path, typer.Option()],
+    min_af: Annotated[float, typer.Option()] = 0.15,
+    min_depth: Annotated[int, typer.Option()] = 2,
+    max_pop_af: Annotated[float, typer.Option()] = 1e-05,
+    max_brca1_func_assay_score: Annotated[float, typer.Option()] = -1.328,
 ) -> None:
     convert_duckdb_to_maf(
-        db_path=db, parquet_dir_path=parquet_dir, out_file_path=out_file
+        db_path=db,
+        parquet_dir_path=parquet_dir,
+        out_file_path=out_file,
+        min_af=min_af,
+        min_depth=min_depth,
+        max_pop_af=max_pop_af,
+        max_brca1_func_assay_score=max_brca1_func_assay_score,
     )
 
 
