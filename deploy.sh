7d8bb3383357eaa7b4d81c93ae2ab5916961e506
Devin McCabe <dmccabe@broadinstitute.org>
2026-01-06 13:31:07 -0500

    fix requirements.txt export

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,14 @@
 
 set -euo pipefail
 
-uv pip compile pyproject.toml -o requirements.txt --emit-index-url > requirements.txt
+uv export \
+  --format requirements.txt \
+  --all-extras \
+  --no-dev \
+  --no-hashes \
+  --no-editable \
+  --no-emit-project \
+  > requirements.txt
 
 echo "Updating GCP Function"
 gcloud functions deploy depmap-omics-wgs \
